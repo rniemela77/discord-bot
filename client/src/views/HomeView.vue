6d24d759fc027a198e823f02814c67dc47267ab4
Robert Niemela <rvniemela@hotmail.com>
2022-06-16 15:13:38 -0700

    refactor: make logging out go back to login screen

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,15 +2,21 @@
 import TaskList from "@/components/task/TaskList.vue";
 
 import { useUserStore } from "@/stores/user";
+import router from "@/router";
 
 const userStore = useUserStore();
+
+const logout = () => {
+  userStore.logout();
+  router.push("/login");
+};
 </script>
 
 <template>
   <div>
     <h1>Task List</h1>
 
-    <button @click="userStore.logout()">Logout</button>
+    <button @click="logout">Logout</button>
 
     <TaskList />
   </div>
