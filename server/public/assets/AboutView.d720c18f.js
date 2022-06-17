1d57e21c22ffb8beabf7e653beff49f9be3555b2
Robert Niemela <rvniemela@hotmail.com>
2022-06-17 12:09:29 -0700

    refactor: build the frontend in order to test on heroku

---
+++ server/public/assets/AboutView.d720c18f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index.12f74801.js";const r={};function t(n,a){return c(),o("h1",null,"About")}var s=e(r,[["render",t]]);export{s as default};
---
+++ server/public/assets/ProfileView.c74b9b40.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c}from"./index.12f74801.js";const o={};function n(t,a){return r(),c("h1",null,"My Profile")}var f=e(o,[["render",n]]);export{f as default};
